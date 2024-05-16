9d1813ebf1fb3f0b2826b0b3f2a3502462d843d8
Danni96 <loddanny9@gmail.com>
2024-05-16 14:51:03 -0600

    'cambios'

--- app/Http/Controllers/ImagenController.php
+++ app/Http/Controllers/ImagenController.php
@@ -2,11 +2,22 @@
 
 namespace App\Http\Controllers;
 
+use Illuminate\Support\Str;
 use Illuminate\Http\Request;
+use Intervention\Image\Facades\Image;
 
 class ImagenController extends Controller
 {
     public function store(Request $request) {
+        $image = $request->file('file');
+
+        $nombreImagen = Str::uuid() . "." . $image->extension();
+    
+        // $imagenServidor = Image::make($image);
         
+        return response();
     }
+
+
+
 }
